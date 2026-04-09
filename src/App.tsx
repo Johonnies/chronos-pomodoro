6f8d1db75bd2862ac97732c89b363688f254511e
João Anacleto <victoranacleto2014@gmail.com>
2026-04-09 14:52:20 -0300

    Start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  console.log("Oi");
+
+  return (
+    <div>
+      <h1>Olá mundo!</h1>
+      <p>Recebe galerinha, vai tomando, ihaa, rapaz!</p>
+    </div>
+  );
+}
+
+export { App };
--- src/assets/App.tsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  console.log("Oi");
-
-  return <h1>Olá Mundo (do APP)!</h1>;
-}
-
-export default App;
